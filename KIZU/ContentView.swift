f6dd20978d7e80c86823c2b5b588e63bb04a5096
Nouf Alshawoosh <Noufshawoosh@gmail.com>
2026-06-21 17:58:34 +0300

    Initial Commit

---
+++ KIZU/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KIZU
+//
+//  Created by Nouf Alshawoosh on 21/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KIZU/KIZUApp.swift
@@ -0,0 +1,17 @@
+//
+//  KIZUApp.swift
+//  KIZU
+//
+//  Created by Nouf Alshawoosh on 21/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct KIZUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
